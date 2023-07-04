927bfe443e30cb0f5ce6eea977c96ded012c89d7
Robson Vilaça <robsonvilaca.lima@gmail.com>
2023-07-03 22:56:05 -0300

    feat: Criando um banco de dados
    Criando Banco de dados sql

---
+++ 1.Backend/NaBrasa-Service/NaBrasa-Service/Migrations/20230704013840_CreateHamburgerMenu.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NaBrasa_Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateHamburgerMenu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HamburguerMenus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BurguerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BurguerDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HamburguerMenus", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HamburguerMenus");
+        }
+    }
+}
