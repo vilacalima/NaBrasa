c69ac6187780cab6ac776979f83e6de97880e584
Robson Vilaça <robsonvilaca.lima@gmail.com>
2023-07-04 23:34:52 -0300

    feat: Create Crud to HamburgerMenu

---
+++ 1.Backend/NaBrasa-Service/NaBrasa-Service/Dao/HamburguerDAO.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NaBrasa_Service.Interfaces;
+using NaBrasa_Service.Model;
+
+namespace NaBrasa_Service.Dao
+{
+    public class HamburguerDAO<H> : IHamburguerDAO<H> where H : HamburguerMenu
+    {
+        private readonly AppDBContext _context;
+
+        public HamburguerDAO(AppDBContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public async Task DeleteHamburguer(int id)
+        {
+            var debug = await _context.HamburguerMenus.FirstOrDefaultAsync(h => h.Id == id);
+            if (debug != null)
+            {
+                _context.HamburguerMenus.Remove(debug);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<H> GetHamburguerById(int id)
+        {
+            return (H)await _context.HamburguerMenus.FirstOrDefaultAsync(h => h.Id == id);
+        }
+
+        public async Task<H> InsertHamburguer(H hamburguer)
+        {
+            var debug = _context.HamburguerMenus.Add(hamburguer);
+            await debug.Context.SaveChangesAsync();
+
+            return hamburguer;
+        }
+
+        public Task<H> UpdateHamburguer(H hamburguer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<HamburguerMenu>> GetListHamburguer()
+        {
+            return await _context.HamburguerMenus.ToListAsync();
+        }
+    }
+}
---
+++ 1.Backend/NaBrasa-Service/NaBrasa-Service/Interfaces/IHamburguerDAO.cs
@@ -0,0 +1,13 @@
+﻿using NaBrasa_Service.Model;
+
+namespace NaBrasa_Service.Interfaces
+{
+    public interface IHamburguerDAO<H> where H : HamburguerMenu
+    {
+        Task<H> GetHamburguerById(int id);
+        Task<H> InsertHamburguer(H hamburguer);
+        Task<H> UpdateHamburguer(H hamburguer);
+        Task DeleteHamburguer(int id);
+        Task<List<HamburguerMenu>> GetListHamburguer();
+    }
+}
